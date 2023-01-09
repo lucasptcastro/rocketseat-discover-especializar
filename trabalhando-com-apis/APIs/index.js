9f4c575c6f8bb8c98bfd695a179b5579f8501bd0
lucasptcastro@gmail.com <lucasptcastro@gmail.com>
2023-01-09 14:25:10 -0300

    🎉 init: init commit

---
+++ trabalhando-com-apis/APIs/index.js
@@ -0,0 +1,8 @@
+// Indica que vai usar o framework express
+const express = require("express");
+
+// Chama a função express
+const app = express();
+
+// Indica a porta em que a aplicação vai rodar
+app.listen("4000");
