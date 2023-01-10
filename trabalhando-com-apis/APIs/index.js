04128560d0cc42ec132325e7c649862522aeb306
lucasptcastro@gmail.com <lucasptcastro@gmail.com>
2023-01-10 09:51:51 -0300

    ✨ feat: added get routes

--- trabalhando-com-apis/APIs/index.js
+++ trabalhando-com-apis/APIs/index.js
@@ -6,3 +6,10 @@ const app = express();
 
 // Indica a porta em que a aplicação vai rodar
 app.listen("4000");
+
+// Cria o conteúdo que será exibido em uma determinada rota
+app.route("/").get((request, response) => response.send("rota base"));
+app.route("/sobre").get((request, response) => response.send("rota nova"));
+app
+  .route("/sobre/lucas")
+  .get((request, response) => response.send("rota dentro de outra"));
