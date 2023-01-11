7c38b32a2dbeb55cc5ba4903f5f516ae9b14946b
lucasptcastro@gmail.com <lucasptcastro@gmail.com>
2023-01-11 16:05:27 -0300

    ✨ feat: added put route

---
+++ trabalhando-com-apis/APIs/put.js
@@ -0,0 +1,22 @@
+// Indica que vai usar o framework express
+const express = require("express");
+
+// Chama a função express
+const app = express();
+
+// Indica a porta em que a aplicação vai rodar
+app.listen("4000");
+
+// Middleware:
+app.use(express.json());
+
+let author = "Lucas";
+
+// Mostra qual o resultado atual da variável "author"
+app.route("/").get((request, response) => response.send(author));
+
+// Recebe o resultado da atualização da variável "author" que foi realizada diretamente na api
+app.route("/").put((request, response) => {
+  author = request.body.author;
+  response.send(author);
+});
