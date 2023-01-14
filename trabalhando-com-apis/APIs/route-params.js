c888f62b5d2431f0fd1140dcbff9ccf7f10c27d1
lucasptcastro@gmail.com <lucasptcastro@gmail.com>
2023-01-13 21:52:08 -0300

    📃 docs: documentation

--- trabalhando-com-apis/APIs/route-params.js
+++ trabalhando-com-apis/APIs/route-params.js
@@ -4,7 +4,10 @@ const app = express();
 
 app.listen("4000");
 
+// Se não for passado um parâmetro, vai retornar "oi"
 app.route("/").get((request, response) => response.send("oi"));
+
+// O parâmetro é passado na rota e mostrado no insomnia através do request.params.variavel
 app
   .route("/:variavel")
   .get((request, response) => response.send(request.params.variavel));
