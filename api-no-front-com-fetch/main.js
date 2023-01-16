c86002043b69bb3cd14e663652b9978aa8680888
lucasptcastro@gmail.com <lucasptcastro@gmail.com>
2023-01-16 16:02:40 -0300

    ✨ feat: created getUser function

--- api-no-front-com-fetch/main.js
+++ api-no-front-com-fetch/main.js
@@ -1,10 +1,22 @@
 const url = "http://localhost:5500/api";
 
-function getUser() {
+function getUsers() {
   fetch(url)
     .then((result) => result.json())
     .then((data) => (renderApiResult.textContent = JSON.stringify(data)))
     .catch((error) => console.error(error));
 }
 
+function getUser() {
+  fetch(`${url}/1`)
+    .then((result) => result.json())
+    .then((data) => {
+      (userName.textContent = data.name),
+        (userCity.textContent = data.city),
+        (userAvatar.src = data.avatar);
+    })
+    .catch((error) => console.error(error));
+}
+
+getUsers();
 getUser();
