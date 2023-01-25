1dd9c0480284cd72e39f6613be4a6c53f5c217a2
lucasptcastro@gmail.com <lucasptcastro@gmail.com>
2023-01-25 17:49:00 -0300

    🦄 refactor: file moved

---
+++ discover-react/reactapp/src/pages/Home/index.tsx
@@ -0,0 +1,9 @@
+export function Home() {
+  return (
+    <>
+      <h1>Lista de presença</h1>
+      <input type="text" placeholder="Digite o nome..." />
+      <button type="button">Adicionar</button>
+    </>
+  );
+}
