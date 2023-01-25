720af7efa86ac7849b36cf9876576f90233da207
lucasptcastro@gmail.com <lucasptcastro@gmail.com>
2023-01-25 17:48:48 -0300

    ✨ feat: import global css

--- discover-react/reactapp/src/main.tsx
+++ discover-react/reactapp/src/main.tsx
@@ -1,4 +1,5 @@
 import { render } from "preact";
-import { Home } from "./pages/home";
+import { Home } from "./pages/Home";
+import "./styles/global.css";
 
 render(<Home />, document.getElementById("app") as HTMLElement);
