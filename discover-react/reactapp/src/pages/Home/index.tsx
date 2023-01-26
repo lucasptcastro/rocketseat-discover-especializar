20449ef9c5bc445973396a3f2e87d871011ef62d
lucasptcastro@gmail.com <lucasptcastro@gmail.com>
2023-01-26 17:13:56 -0300

    🌈 style: applied styling

--- discover-react/reactapp/src/pages/Home/index.tsx
+++ discover-react/reactapp/src/pages/Home/index.tsx
@@ -1,9 +1,11 @@
+import "./style.css";
+
 export function Home() {
   return (
-    <>
+    <div className="container">
       <h1>Lista de presença</h1>
       <input type="text" placeholder="Digite o nome..." />
       <button type="button">Adicionar</button>
-    </>
+    </div>
   );
 }
