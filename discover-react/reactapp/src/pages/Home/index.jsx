89d3ef80a9770ebaa43d72ade5604f5392ff027e
lucasptcastro@gmail.com <lucasptcastro@gmail.com>
2023-01-27 12:09:43 -0300

    ✨ feat: added state for the student name

--- discover-react/reactapp/src/pages/Home/index.jsx
+++ discover-react/reactapp/src/pages/Home/index.jsx
@@ -1,11 +1,21 @@
+import React, { useState } from "React";
 import { Card } from "../../components/Card";
 import "./style.css";
 
 export function Home() {
+  const [studentName, setStudentName] = useState("");
+
   return (
     <div className="container">
-      <h1>Lista de presença</h1>
-      <input type="text" placeholder="Digite o nome..." />
+      <h1>Nome: {studentName}</h1>
+
+      {/* O 'onChange' serve para pegar cada atualização do input através do e.target.value */}
+      <input
+        type="text"
+        placeholder="Digite o nome..."
+        onChange={(e) => setStudentName(e.target.value)}
+      />
+
       <button type="button">Adicionar</button>
 
       <Card name="Tom Riddle" time="07:01:14" />
