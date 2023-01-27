5e878561663bb22c42f95bd2386cfcd67b543bbc
lucasptcastro@gmail.com <lucasptcastro@gmail.com>
2023-01-27 10:26:26 -0300

    ✨ feat: created card component

---
+++ discover-react/reactapp/src/components/Card/index.jsx
@@ -0,0 +1,10 @@
+import "./style.css";
+
+export function Card() {
+  return (
+    <div className="card">
+      <strong>Lucas Peixoto de Castro</strong>
+      <small>17:05:24</small>
+    </div>
+  );
+}
