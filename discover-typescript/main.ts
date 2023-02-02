f922df055ef5cdd46415a968b23f99dba56b5491
lucasptcastro@gmail.com <lucasptcastro@gmail.com>
2023-02-01 21:25:33 -0300

    ✨ feat: added "any" type

--- discover-typescript/main.ts
+++ discover-typescript/main.ts
@@ -3,9 +3,10 @@
 "user: string | null" = o "user" pode ser string ou nulo
 "user ?? "Usário padrão"" = se o "user" for nulo, vai retornar "Usuário padrão"
 
+any: aceita qualquer tipagem
 */
 
-function showTicket(user: string | null, ticket: number) {
+function showTicket(user: string | null, ticket: any) {
   console.log(
     `Olá ${user ?? "Usário padrão"}. Seu número de bilhete é ${ticket}`
   );
