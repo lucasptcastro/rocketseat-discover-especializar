364d29b080524c5afc84c974002bc34cd7192692
lucasptcastro@gmail.com <lucasptcastro@gmail.com>
2023-02-02 21:32:16 -0300

    ✨ feat: added inferred typing

--- discover-typescript/main.ts
+++ discover-typescript/main.ts
@@ -1,9 +1,10 @@
-/* 
+/*  ==========================================================
 
 "user: string | null" = o "user" pode ser string ou nulo
 "user ?? "Usário padrão"" = se o "user" for nulo, vai retornar "Usuário padrão"
 
 any: aceita qualquer tipagem
+
 */
 
 function showTicket(user: string | null, ticket: any) {
@@ -11,5 +12,14 @@ function showTicket(user: string | null, ticket: any) {
     `Olá ${user ?? "Usário padrão"}. Seu número de bilhete é ${ticket}`
   );
 }
-
 showTicket(null, 123);
+
+/* ==========================================================
+
+Tipagem explícita: indica a tipagem ao lado da variável
+Tipagem inferida: indica a tipagem no valor da variável
+
+*/
+
+let user: string = "Lucas"; // Tipagem explícita
+let newUser: "Felipe"; // Tipagem inferida
