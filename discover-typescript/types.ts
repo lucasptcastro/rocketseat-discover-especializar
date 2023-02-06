68762c2fcc4062433f1d352cf59e08ed1be95797
lucasptcastro@gmail.com <lucasptcastro@gmail.com>
2023-02-06 09:24:39 -0300

    🦄 refactor: refactor

--- discover-typescript/types.ts
+++ discover-typescript/types.ts
@@ -1,53 +1,46 @@
-/* ==========================================================
+("==========================================================");
 
-    Tipagem explícita: indica a tipagem ao lado da variável
-    Tipagem inferida: indica a tipagem no valor da variável
-
-*/
+//  Tipagem explícita: indica a tipagem ao lado da variável
+//  Tipagem inferida: indica a tipagem no valor da variável
 
 let user: string = "Lucas"; // Tipagem explícita
 let newUser: "Felipe"; // Tipagem inferida
 
-/* ==========================================================
+("==========================================================");
 
-    Tipos primitivos:
-
-*/
+//  Tipos primitivos:
 
 let loading: boolean = false;
 let email: string = "teste@gmail.com";
 let prince: number = 10.5;
 
-/* ==========================================================
+("==========================================================");
 
-Matrizes (dá para usar das duas formas abaixo):
-
-*/
+//  Matrizes (dá para usar das duas formas abaixo):
 
 let numbers: number[] = [1, 2, 3, 4, 5];
 let users: Array<string> = ["Lucas", "Felipe", "Vanessa"];
 
-/* ==========================================================
+("==========================================================");
 
-Funções:
+//  Funções:
 
-*/
-
-// O tipo "void" significa que a função não tem retorno
+//  O tipo "void" significa que a função não tem retorno
 function showMessage(message: string): void {
   console.log(message);
 }
-// O tipo "number" significa que a função vai retornar  um número
+//  O tipo "number" significa que a função vai retornar  um número
 function sumNumbers(a: number, b: number): number {
   return a + b;
 }
 
-/* ==========================================================
+("==========================================================");
 
-O operador union "|" serve como um "ou", ou seja:
-o parametro "id" pode receber um number ou uma string
+//  O operador union "|" serve como um "ou", ou seja:
+//  o parametro "id" pode receber um number ou uma string
 
-*/
 function printUserId(id: number | string) {
   console.log(`O ID do usuário é: ${id}`);
 }
+
+("==========================================================");
