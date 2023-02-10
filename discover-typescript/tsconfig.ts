220e3c05bcad8c81b998902f40a6b66ffaa1ba79
lucasptcastro@gmail.com <lucasptcastro@gmail.com>
2023-02-09 21:04:48 -0300

    ✨ feat: tsconfig informations

---
+++ discover-typescript/tsconfig.ts
@@ -0,0 +1,8 @@
+/*
+
+Desabilitar o erro de any que aparece ao deixar uma variável sem tipagem:
+
+    - No arquivo "tsconfig.json", basta deixar o "noImplicitAny": true"
+    como "false". 
+    
+*/
